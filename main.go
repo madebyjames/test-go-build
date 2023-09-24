21bb88a9924583c455982b697f43f9c41a7d8b65
James Smith <8142061+madebyjames@users.noreply.github.com>
2023-09-24 23:45:07 +0100

    feat: write a file (#1)

--- main.go
+++ main.go
@@ -2,8 +2,27 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
 )
 
 func main() {
+	e, err := os.Executable()
+	if err != nil {
+		panic(err)
+	}
+	ep := filepath.Dir(e)
+
+	file, err := os.Create(fmt.Sprintf("%s/test.txt", ep))
+	defer file.Close()
+	if err != nil {
+		panic(err)
+	}
+
+	_, err = file.WriteString("Test text")
+	if err != nil {
+		panic(err)
+	}
+
 	fmt.Println("complete")
 }
